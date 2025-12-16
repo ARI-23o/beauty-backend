288f5dbab5261a29a62754efa4133921ace5d40f
ARI-23o <cs2022266@global.org.in>
2025-12-16 10:26:05 +0530

    Update favoriteRoutes.js

--- routes/favoriteRoutes.js
+++ routes/favoriteRoutes.js
@@ -1,6 +1,6 @@
 // server/routes/favoriteRoutes.js
 import express from "express";
-import { verifyToken } from "../middleware/verifyToken.js";
+import  verifyToken  from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
